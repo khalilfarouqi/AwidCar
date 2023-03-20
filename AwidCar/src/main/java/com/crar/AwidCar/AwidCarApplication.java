eb77b6497ca3d144626e85b03003c8ce16d96c4b
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-20 17:51:32 +0000

    initial commit

---
+++ AwidCar/src/main/java/com/crar/AwidCar/AwidCarApplication.java
@@ -0,0 +1,13 @@
+package com.crar.AwidCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwidCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwidCarApplication.class, args);
+	}
+
+}
---
+++ AwidCar/src/test/java/com/crar/AwidCar/AwidCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crar.AwidCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwidCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a013a2ff8d57c047e0d5b585b48587582e178d3f
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-22 09:14:47 +0000

    Add Entity

---
+++ AwidCar/src/main/java/com/crar/AwidCar/entity/Notification.java
@@ -0,0 +1,29 @@
+package com.crar.AwidCar.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="Notifications")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String object;
+
+    @Column(columnDefinition = "NVARCHAR(MAX)")
+    private String body;
+
+    private Boolean isRead;
+
+    private Boolean notificationWeb;
+
+    @ManyToOne
+    private User user;
+
+    @OneToOne
+    private NotificationGroup notificationGroup;
+}

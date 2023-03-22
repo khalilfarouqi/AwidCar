33183cd3329593d3df2e1aed66a9d247a4991189
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-22 14:59:33 +0000

    Add extend of repository

--- AwidCar/src/main/java/com/crar/AwidCar/repository/NotificationGroupRepository.java
+++ AwidCar/src/main/java/com/crar/AwidCar/repository/NotificationGroupRepository.java
@@ -1,7 +1,9 @@
 package com.crar.AwidCar.repository;
 
+import com.crar.AwidCar.entity.NotificationGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationGroupRepository {
+public interface NotificationGroupRepository extends JpaRepository<NotificationGroup, Long> {
 }
--- AwidCar/src/main/java/com/crar/AwidCar/repository/OrderRepository.java
+++ AwidCar/src/main/java/com/crar/AwidCar/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.crar.AwidCar.repository;
 
+import com.crar.AwidCar.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }

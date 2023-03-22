c4d80405985359e8ca2fdfcadd91e34260ce9c21
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-22 09:46:48 +0000

    Add Entity Dto

---
+++ AwidCar/src/main/java/com/crar/AwidCar/dto/ClientDto.java
@@ -0,0 +1,27 @@
+package com.crar.AwidCar.dto;
+
+import com.crar.AwidCar.entity.enums.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.*;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ClientDto {
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private int age;
+    private String adress;
+    private String tel;
+    private City city;
+    private String country;
+    private String cin;
+    private String licence;
+    private Date dateLicence;
+    private Date dateCreation;
+    private String email;
+    private int volNumbre;
+}

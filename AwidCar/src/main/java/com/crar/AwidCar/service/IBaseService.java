4c225c3e1d54fed8446910eb66fd33f19a39cc96
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-22 16:15:36 +0000

    Add Base

---
+++ AwidCar/src/main/java/com/crar/AwidCar/service/IBaseService.java
@@ -0,0 +1,13 @@
+package com.crar.AwidCar.service;
+
+import java.io.Serializable;
+
+public interface IBaseService<E, D extends Serializable> extends IRsqlService<E, D> {
+    D save(D dto);
+
+    D update(D dto);
+
+    void delete(Long id);
+
+    D findById(Long id);
+}

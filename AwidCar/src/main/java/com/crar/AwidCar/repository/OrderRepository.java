f9aaa15d1591a1063b75b41e1348dc3a5ffc3fb1
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-22 17:22:33 +0000

    FindAll in Repository

--- AwidCar/src/main/java/com/crar/AwidCar/repository/OrderRepository.java
+++ AwidCar/src/main/java/com/crar/AwidCar/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.crar.AwidCar.repository;
 
 import com.crar.AwidCar.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAll(Specification spec, Pageable pageable);
 }

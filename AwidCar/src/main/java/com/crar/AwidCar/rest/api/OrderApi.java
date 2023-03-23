3f4752977289a0b13e05404d553ecf6d8e335eea
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-23 11:53:19 +0000

    Add CRUD of Order

--- AwidCar/src/main/java/com/crar/AwidCar/rest/api/OrderApi.java
+++ AwidCar/src/main/java/com/crar/AwidCar/rest/api/OrderApi.java
@@ -1,11 +1,30 @@
 package com.crar.AwidCar.rest.api;
 
+import com.crar.AwidCar.entity.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @Tag(name = "Order", description = "REST API for Order information")
 @RequestMapping("/v1/order")
 public interface OrderApi {
+    @PostMapping
+    Order save(@RequestBody Order order);
+    @PutMapping
+    Order update(@RequestBody Order order);
+    @DeleteMapping(value = "/{id}")
+    void delete(@PathVariable Long id);
+    @GetMapping(value = "/{id}")
+    Order getOrderById(@PathVariable("id") Long id);
+    @GetMapping(value = "/getAll")
+    List<Order> getAllOrder();
+    @GetMapping(value = "/search")
+    Page<Order> search(@RequestParam(defaultValue = "id>0") String query,
+                      @RequestParam(defaultValue = "0") Integer page,
+                      @RequestParam(defaultValue = "10") Integer size,
+                      @RequestParam(defaultValue = "asc") String order,
+                      @RequestParam(defaultValue = "id") String sort);
 }
--- AwidCar/src/main/java/com/crar/AwidCar/rest/controller/OrderController.java
+++ AwidCar/src/main/java/com/crar/AwidCar/rest/controller/OrderController.java
@@ -1,12 +1,47 @@
 package com.crar.AwidCar.rest.controller;
 
+import com.crar.AwidCar.entity.Order;
 import com.crar.AwidCar.rest.api.OrderApi;
+import com.crar.AwidCar.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class OrderController implements OrderApi {
+    private final OrderService orderService;
+    @Override
+    public Order save(Order order) {
+        return orderService.save(order);
+    }
+
+    @Override
+    public Order update(Order order) {
+        return orderService.update(order);
+    }
+
+    @Override
+    public void delete(Long id) {
+        orderService.delete(id);
+    }
+
+    @Override
+    public Order getOrderById(Long id) {
+        return orderService.findById(id);
+    }
+
+    @Override
+    public List<Order> getAllOrder() {
+        return orderService.findAll();
+    }
+
+    @Override
+    public Page<Order> search(String query, Integer page, Integer size, String order, String sort) {
+        return orderService.rsqlQuery(query, page, size, order, sort);
+    }
 }

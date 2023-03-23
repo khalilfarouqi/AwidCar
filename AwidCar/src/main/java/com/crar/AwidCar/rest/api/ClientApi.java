45a6c46d00863b3ad92eef461483b2ba0689ab1c
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-23 11:52:29 +0000

    Add CRUD of Client

--- AwidCar/src/main/java/com/crar/AwidCar/rest/api/ClientApi.java
+++ AwidCar/src/main/java/com/crar/AwidCar/rest/api/ClientApi.java
@@ -1,11 +1,30 @@
 package com.crar.AwidCar.rest.api;
 
+import com.crar.AwidCar.entity.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @Tag(name = "Client", description = "REST API for client information")
 @RequestMapping("/v1/client")
 public interface ClientApi {
+    @PostMapping
+    Client save(@RequestBody Client client);
+    @PutMapping
+    Client update(@RequestBody Client client);
+    @DeleteMapping(value = "/{id}")
+    void delete(@PathVariable Long id);
+    @GetMapping(value = "/{id}")
+    Client getClientById(@PathVariable("id") Long id);
+    @GetMapping(value = "/getAll")
+    List<Client> getAllClient();
+    @GetMapping(value = "/search")
+    Page<Client> search(@RequestParam(defaultValue = "id>0") String query,
+                       @RequestParam(defaultValue = "0") Integer page,
+                       @RequestParam(defaultValue = "10") Integer size,
+                       @RequestParam(defaultValue = "asc") String order,
+                       @RequestParam(defaultValue = "id") String sort);
 }

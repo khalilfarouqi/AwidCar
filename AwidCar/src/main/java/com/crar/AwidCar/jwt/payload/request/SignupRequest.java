7907f960778a3886838134fb4e219fc9409b3037
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-29 09:30:03 +0000

    Add SignIn and SignUp classes

---
+++ AwidCar/src/main/java/com/crar/AwidCar/jwt/payload/request/SignupRequest.java
@@ -0,0 +1,22 @@
+package com.crar.AwidCar.jwt.payload.request;
+
+import com.crar.AwidCar.entity.enums.Roles;
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class SignupRequest {
+    @NotNull
+    private String nom;
+    @NotNull
+    private String prenom;
+    @NotNull
+    private String username;
+    @NotNull
+    private Roles roles;
+    private String login;
+    @NotBlank
+    private String password;
+}
